11d961c3ac1d5408d91b3c0c4c54f98de94383e0
Leonidius20 <25536411+Leonidius20@users.noreply.github.com>
2024-04-25 13:12:59 +0300

    init

---
+++ src/main/java/io/github/leonidius20/twitterclone/authservice/TwitterAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package io.github.leonidius20.twitterclone.authservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterAuthServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwitterAuthServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/leonidius20/twitterclone/authservice/TwitterAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.leonidius20.twitterclone.authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterAuthServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
